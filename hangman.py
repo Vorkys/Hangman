021d70b284c2c258f7ee1d35178ddafb7862a10b
Vorkys <75934309+Vorkys@users.noreply.github.com>
2021-01-15 15:50:59 +0100

    Update hangman.py
    fixed language mistakes
    translated words from czech to english

--- hangman.py
+++ hangman.py
@@ -1,8 +1,9 @@
 import random
 
-#this is the base list of words that can be picked by the program, added words by the player (look for "ADD" at the bottom) are stocked here until reseted
-words = ["kotrmelec", "olovo", "kniha", "kocour", "had", "objektiv", "neandrtalec", "opice", "podlozka", "mys", "mikrofon", "zavinac", "klavesnice", "disketa", "skateboard", "algoritmus",
-         "oko", "koule", "programovani", "zaclony", "okno", "ucebnice", "pivo", "alkohol", "lod", "tabor", "trubadur", "housle", "kolobezka", "houpacka", "rucnik", "zrcadlo", "perina"]
+#list of words the player can get
+words = ["pen", "iron", "book", "cat", "snake", "objective", "human", "monkey", "car", "mouse", "microphone", "hashtag", "keyboard", "disc", "skateboard", "algorithm",
+         "eye", "ball", "programing", "curtains", "window", "teacher", "beer", "alcohol", "ship", "camp", "shop", "violin", "scooter", "swing", "towel", "mirror", "duvet",
+         "computer", "sport", "desk", "money", "virtual", "faith", "cactus", "chocolate", "sweet", "comics", "radiator", "loan", "leak", "screen"]
 def game():
     """this handles the whole "guess the word" part
     I will split it into 2 parts.
@@ -22,10 +23,10 @@ def game():
     lives = 8
     correct = 0
     lost = False
-    while correct != len(word):                                                 #while and correct are handling if the word is guessed or not
+    while correct != len(word):                                                 #while the word isnt complete
         print(52 * "=")
 
-        if lives == 0:                                                          #if 0 lives it ends the game and reveals the word
+        if lives == 0:                                                          #no lives left -> it ends the game and reveal the word
             print("You lost. The word was: ")
             print(" ".join(map(str, word)))
             lost = True
@@ -46,20 +47,20 @@ def game():
 
         position = 0
         bad = True
-        if guess_up in letters and len(guess) == 1:                 #checks if there is the letter in the word if not it removes 1 health
+        if guess_up in letters and len(guess) == 1:                 #letter not in word -> -1 health
             for _letter in word:
                 if _letter == guess_down:                           #if the letter is in the world it reveals the letter and add it to the used_letters list aswel as removing it in the letter that are able to be used
                     correct += 1
                     word_guess[position] = word[position]
                     bad = False
 
-                    if guess_up in letters:                         #this if is handling if there are many letter that are the same in the word
+                    if guess_up in letters:                         #needed if the letter is in word more than once otherwise it crashes
                         letters.remove(guess_up)
                         used_letters.append(guess_up)
 
                 position += 1
             
-            if bad == True:                                         #this if is called if no letter is equal while going through the word
+            if bad == True:                                         #if the letter isnt in the word
                 lives -= 1
                 print("Wrong. You lost 1 health.")
 
@@ -68,12 +69,12 @@ def game():
 
             position = 0
 
-        elif guess_up == "STOP":                                    #if the player decide to stop at any moment it reveals the word and asks if he wishes to play a new game
+        elif guess_up == "STOP":                                    #stops the current game
             print("The word was: ")
             print(" ".join(map(str, word))) 
             lost = True
             break
-        else:                                                       #input isnt 1 letter or in the list of pickable letters - will reask the player while also reshowing the "game"
+        else:                                                       #input isnt one letter or in the list of pickable letters - will reask the player while also reshowing the "game"
             print("invalid input")
 
     if lost == False:
@@ -82,19 +83,20 @@ def game():
 
 
 def play_game():
-    n = input("Add a word(ADD) | New game(1/ano/yes): ")
+    n = input("Add a word(add) | New game(1/ano/yes): ")
     start = n.upper()
-    if start == "1" or start == "ANO" or start == "YES":                #asks if the player wish to play another game with another random word
+    if start == "1" or start == "ANO" or start == "YES":                #asks if the player wish to play a new game
         game()
         play_game()
-    elif start == "ADD":                                                #gives the player the possibility to add a word of his own
+    elif start == "ADD":                                                #player can add a word
         n = input("Word you wish to add: ")
         words.append(n)
         play_game()
     else:                                                               #ends the program
         print("Game over.")
 
-print("Rules are & informations: ")
+#rules#
+print("Rules & informations: ")
 print("1) diacritics arent counted")
 print("2) should be possible to play infinitely")
 print("3) words, which the player adds wont be keeped after restart(WIP to even remove a word)")
